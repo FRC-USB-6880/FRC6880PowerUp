b92474fb920f071edaa1229feedc7ccaa9a96d5f
Pranav Burugula <pranav.burugula@gmail.com>
2018-01-22 15:48:35 -0500

    added gamepad

--- src/org/usfirst/frc/team6880/robot/FRCRobot.java
+++ src/org/usfirst/frc/team6880/robot/FRCRobot.java
@@ -3,11 +3,13 @@ package org.usfirst.frc.team6880.robot;
 import org.usfirst.frc.team6880.robot.driveTrain.DriveSystem;
 import org.usfirst.frc.team6880.robot.navigation.Navigation;
 import org.usfirst.frc.team6880.robot.task.*;
+import org.usfirst.frc.team6880.robot.util.LogitechF310;
 
 public class FRCRobot {
 	Robot wpilibrobot;
 	public DriveSystem driveSys;
 	public Navigation navigation;
+	public LogitechF310 gamepad;
 	
 	RobotTask curTask;
 	RobotTask tasks [] = {new TaskSpinCCW90deg(this), new TaskMoveForward20m(this)};
@@ -19,12 +21,14 @@ public class FRCRobot {
 		this.wpilibrobot = wpilibrobot;
 		this.driveSys = new DriveSystem(this);
 		navigation = new Navigation(this);
+		gamepad = new LogitechF310(0);
 	}
 	
 	public void runTeleOp()
 	{
 		//TODO: Map controller sticks to drive system
 		//Possible: map misc. controller buttons to tasks?
+		driveSys.arcadeDrive(gamepad.leftStickY(), gamepad.rightStickX());
 	}
 	
 	public void initAutonomous()
