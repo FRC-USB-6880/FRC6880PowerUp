cc48be8085bfc71ce2f88b1f401d153e3feb4151
ram4robotics <burugula@gmail.com>
2018-01-30 13:46:36 -0500

    Added empty coordinate system class

---
+++ src/org/usfirst/frc/team6880/robot/navigation/CoordinateSys.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team6880.robot.navigation;
+
+/**
+ * TODO
+ *
+ */
+public class CoordinateSys {
+    
+    double starting_x, starting_y, starting_theta;
+    double current_x, current_y, current_theta;
+
+    /**
+     * 
+     */
+    public CoordinateSys(double starting_x, double starting_y, double starting_theta) {
+        // TODO Auto-generated constructor stub
+        // This will be invoked during robotInit()
+    }
+    
+    public void setOrigin() {
+        // TODO
+        // Set the current pose as (x = 0, y = 0, theta = 0)
+        // Get the gyro reading and encoder counts
+        // This will be called during autonomousInit()
+    }
+    
+    public void displayCurrentPose() {
+        // TODO
+        
+    }
+
+}
