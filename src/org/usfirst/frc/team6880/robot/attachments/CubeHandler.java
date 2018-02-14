4499f1ea91c8e19f0c2b81f152be7e5905c66b1a
Janani Rajadurai <janani.rajadurai@gmail.com>
2018-02-14 15:38:24 -0500

    Finished CubeHandler Janani

--- src/org/usfirst/frc/team6880/robot/attachments/CubeHandler.java
+++ src/org/usfirst/frc/team6880/robot/attachments/CubeHandler.java
@@ -2,19 +2,41 @@
  * 
  */
 package org.usfirst.frc.team6880.robot.attachments;
+import org.usfirst.frc.team6880.robot.FRCRobot;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 
 /**
  * TODO
  * add methods grabCube(), releaseCube(), ....
  *
  */
-public class CubeHandler {
-
+public class CubeHandler
+{
+	DoubleSolenoid cubeHandler;
+	FRCRobot robot;
     /**
      * 
      */
-    public CubeHandler() {
+    public CubeHandler(FRCRobot robot)
+    {
         // TODO Auto-generated constructor stub
+    	this.robot = robot;
+    	cubeHandler = robot.pcmObj.initializeDoubleSolenoid(0, 1);
+        cubeHandler.set(Value.kOff);
     }
-
+    
+    public void grabCube()
+    {
+    	cubeHandler.set(DoubleSolenoid.Value.kForward);
+    }
+    public void releaseCube()
+    {
+    	cubeHandler.set(DoubleSolenoid.Value.kReverse);
+    }
+    
+    public void idle()
+    {
+    	cubeHandler.set(DoubleSolenoid.Value.kOff);
+    }
 }
--- src/org/usfirst/frc/team6880/robot/attachments/Lift.java
+++ src/org/usfirst/frc/team6880/robot/attachments/Lift.java
@@ -2,18 +2,63 @@
  * 
  */
 package org.usfirst.frc.team6880.robot.attachments;
+import org.usfirst.frc.team6880.robot.FRCRobot;
+import org.usfirst.frc.team6880.robot.jsonReaders.WheelSpecsReader;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
 /**
  * TODO
  *
  */
-public class Lift {
-
+//motor, FRC robot, methods for raising and lowering lift, 
+public class Lift
+{
+	FRCRobot robot;
+	WPI_TalonSRX liftMotor;
+	private double height;
+	public Encoder liftEncoder;
+	
+	private double spoolDiameter;
+	private double spoolCircumference;
+	private double distancePerCount;
     /**
      * 
      */
-    public Lift() {
+    public Lift(FRCRobot robot)
+    {
         // TODO Auto-generated constructor stub
+    	this.robot = robot;
+    	liftMotor = new WPI_TalonSRX(4);
+    	height = 0;
+    	liftEncoder = new Encoder(4, 5, true, Encoder.EncodingType.k4X);
+    	spoolDiameter = 2;
+    	spoolCircumference = Math.PI * spoolDiameter;
+    	distancePerCount = spoolCircumference / 360;
+    	liftEncoder.setDistancePerPulse(distancePerCount);
+    }
+    
+    public void stop()
+    {
+    	liftMotor.set(0);
+    }
+    
+    public void moveWithPower(double power)
+    {
+    	liftMotor.set(power);
+    }
+    
+    public void moveToHeight(double targetHeight, double power)
+    {
+    	double amountRaise = targetHeight - height;
+    	if(liftEncoder.getDistance() != amountRaise)
+    	{
+    		if(amountRaise < 0) moveWithPower(-power);
+    		else moveWithPower(power);
+    	}
+    	else stop();
+    	height += liftEncoder.getDistance();
     }
 
 }
